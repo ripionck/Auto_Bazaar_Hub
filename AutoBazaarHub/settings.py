ddc1ff102119e8c6a76c9d3df9131567255b8fd6
Ripion Chakma <ripionchakma.421@gmail.com>
2023-12-17 23:52:24 +0600

    added static image and text on home page

--- AutoBazaarHub/settings.py
+++ AutoBazaarHub/settings.py
@@ -125,6 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+         BASE_DIR/'static']
 
 # Base url to serve media files
 MEDIA_URL = '/media/'
